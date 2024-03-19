0d63ed0b0e429d84840d7c6001fec18fd85b3d3e
frondendasoslar <faxriddin19951021@gmai.com>
2024-03-19 08:31:16 +0500

    new js coding

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const elMenubtn = document.querySelector('.menu-btn');//link klasi tobib olinayapdi
 const elSpan = document.querySelector('.span');//span klasi topib ilinayapdi
-const elNavMenu = document.querySelector('.nav-menu');
+const elNavMenu = document.querySelector('.nav-menu');//Linklarni ota elementi klasini topib olayapdi
 
 
 //link bosilganda spanning klasiga yangi klas qo'shish uchun kiritilayapdi
 elMenubtn.addEventListener('click', function() {//link klasi click hodisasiga quloq solayapdi va funktsiya kiritilayapdi
   elSpan.classList.toggle('span-btn');//link bosilganda spanning klasiga yangi klas qo'shilayapdi
-  elNavMenu.classList.toggle('nav-menu-active');
+  elNavMenu.classList.toggle('nav-menu-active');//linklarni o'rab turuvchi ota elementning klaslistiga nav-menu-active klasini qo'shib qo'yayapdi
 })
